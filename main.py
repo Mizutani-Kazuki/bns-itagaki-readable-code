e144b1ad8dcddbd88103516b3faaed59515f62cf
bns-itagaki <itagaki_tatsuya@avilen.co.jp>
2022-11-02 12:06:32 +0900

    spec5: 個々の単語にIDを割り振る

--- main.py
+++ main.py
@@ -7,14 +7,21 @@ def main():
     単語情報の出力
     入力ファイルから単語情報を読み込んでターミナル上に出力
     """
+    words_list = [] # 単語情報を保持するリスト
+
     # 入力ファイルを開く
     with open(INPUT_DICTIONARY_DATA_PATH, encoding='utf-8') as input_file:
         # 入力ファイルのテキストを1行ずつrowに格納して処理
         for row in input_file:
             # 入力ファイルはcsvを想定しているので','区切りで取り出す
-            word_list = row.strip().split(',')
-            # 入力ファイルから読み込んだ単語を画面に出力
-            print(word_list[0])
+            word_info = row.strip().split(',')
+            # 入力ファイルから読み込んだ単語をリストに格納
+            words_list.append(word_info)
+
+    # 取得した単語情報の出力
+    for index, word_info in enumerate(words_list):
+        # リストの添え字+1をIDとして出力する
+        print(index + 1, ': ', word_info[0], sep='')
 
 
 if __name__ == '__main__':
