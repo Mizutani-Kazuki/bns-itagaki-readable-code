2566854edf5f93755ee3289ba1024cbfc79a8e79
bns-itagaki <itagaki_tatsuya@avilen.co.jp>
2022-11-02 11:27:17 +0900

    spec3: 単語データを別ファイルに分離

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
+# 入力ファイルのパス
+INPUT_DICTIONARY_DATA_PATH = "./dictionary-data.csv"
+
+
 def main():
-    print("上手")
+    """
+    単語情報の出力
+    入力ファイルから単語情報を読み込んでターミナル上に出力
+    """
+    # 入力ファイルを開く
+    with open(INPUT_DICTIONARY_DATA_PATH, encoding='utf-8') as input_file:
+        # 入力ファイルのテキストを1行ずつrowに格納して処理
+        for row in input_file:
+            # 入力ファイルはcsvを想定しているので','区切りで取り出す
+            word_list = row.split(',')
+            # 入力ファイルから読み込んだ単語を画面に出力
+            print(word_list[0])
 
 
 if __name__ == '__main__':
+    """
+    main関数の実行
+    """
     main()
